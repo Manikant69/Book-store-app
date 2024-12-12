b0900627266c56b1ec512f2c6716f9dfc78bf1f7
Manikant Singh <manikant17082001@gmail.com>
2024-12-12 23:38:17 +0530

    changes in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 
 import bookRoute from './route/book.route.js';
 import userRoute from './route/user.route.js'
-const app = express()
+const app = express();
 
 
 app.use(cors());
