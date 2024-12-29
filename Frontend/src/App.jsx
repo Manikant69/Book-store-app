2763de5bca61b307e79b55988de6f82c9f4037a6
Manikant Singh <manikant17082001@gmail.com>
2024-12-29 21:31:26 +0530

    changes in app.js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './home/Home';
 import Courses from './courses/Courses';
-import {Route, Routes, useNavigate } from 'react-router-dom';
+import {Navigate, Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup';
 import Contacts from './contact/Contacts'
 
@@ -11,13 +11,12 @@ import { useAuth } from './context/AuthProvider';
 
 function App() {
   const [authUser, setAuthUser] = useAuth();
-  const navigate = useNavigate();
 
   return (
     <div className='bg-white text-black dark:bg-slate-900 dark:text-white'>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/course' element={authUser ? <Courses/> : navigate('/signup')} />
+      <Route path='/course' element={authUser ? <Courses/> : <Navigate to={'/signup'}/>} />
       <Route path='/signup' element={<Signup/>} />
       <Route path='/contact' element={<Contacts/>}/>
     </Routes>
