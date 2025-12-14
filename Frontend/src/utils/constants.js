3a35235608fc0558769dd282b7b9f677c55d979d
MANIKANT SINGH <100368724+Manikant69@users.noreply.github.com>
2025-12-14 23:02:39 +0530

    Update API endpoints to new URLs

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
-export const USER_API_END_POINT="https://book-store-app-amber.vercel.app/api/users";
-export const BOOK_API_END_POINT="https://book-store-app-amber.vercel.app/api/books";
-export const GENRE_API_END_POINT="https://book-store-app-amber.vercel.app/api/genres";
-export const CART_API_END_POINT="https://book-store-app-amber.vercel.app/api/cart";
-export const WISHLIST_API_END_POINT="https://book-store-app-amber.vercel.app/api/wishlist";
-export const ORDER_API_END_POINT="https://book-store-app-amber.vercel.app/api/orders";
+export const USER_API_END_POINT="https://bookspot-1vwv.onrender.com/api/users";
+export const BOOK_API_END_POINT="https://bookspot-1vwv.onrender.com/api/books";
+export const GENRE_API_END_POINT="https://bookspot-1vwv.onrender.com/api/genres";
+export const CART_API_END_POINT="https://bookspot-1vwv.onrender.com/api/cart";
+export const WISHLIST_API_END_POINT="https://bookspot-1vwv.onrender.com/api/wishlist";
+export const ORDER_API_END_POINT="https://bookspot-1vwv.onrender.com/api/orders";
